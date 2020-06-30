64adb1e2f88a15f57b91a659709ed85dfa7b6500
Roman Miniv <1pi15bminivroman@gmail.com>
2020-06-30 09:05:59 +0300

    Release v2.

---
+++ index.js
@@ -0,0 +1,66 @@
+const
+  input = document.getElementById('input'),
+  output = document.getElementById('output');
+
+input.focus();
+
+input.addEventListener('input', (e) => {
+  const obj = JSON.parse(e.target.value);
+  iterateObject(obj);
+
+  output.value = JSON.stringify(obj, null, 4);
+  output.select();
+});
+
+const
+  acronyms = ['ID', 'URL', 'JSON', 'HTML', 'PDF'],
+  articles = ['at', 'by', 'to', 'on', 'in', 'of', 'for'];
+
+function iterateObject(data) {
+  if (Array.isArray(data)) {
+      data.forEach(item => {
+          addLabel(item);
+          for (let key in item) {
+              if (typeof item[key] === 'object') {
+                iterateObject(item[key]);
+              }
+          }
+      });
+  } else if (typeof data === 'object') {
+      addLabel(data);
+      for (let key in data) {
+          if (typeof data[key] === 'object') {
+            iterateObject(data[key]);
+          }
+      }
+  }
+}
+function addLabel(obj) {
+  const { name } = obj;
+  if (!name) {
+      return obj;
+  }
+  
+  obj.label = name
+    .split('_')
+    .map(word => word.replace(/[A-Z]/g, match => ' ' + match))
+    .join(' ')
+    .trim()
+    .split(' ')
+    .map(word => modifyWordForLabel(word))
+    .join(' ');
+
+  return obj;
+}
+function modifyWordForLabel(word) {
+  if (articles.includes(word.toLowerCase())) {
+    return word.toLowerCase();
+  } else if (acronyms.includes(word.toUpperCase())) {
+    return word.toUpperCase();
+  } else {
+    return capitalizeFirstLetter(word);
+  }
+}
+function capitalizeFirstLetter(string) {
+  return string[0].toUpperCase() + string.slice(1);
+}
