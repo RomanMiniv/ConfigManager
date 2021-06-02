42d0911d60545e65366560f54de795bb3ea5955a
Roman Miniv <1pi15bminivroman@gmail.com>
2021-06-02 22:18:07 +0300

    Added acronyms.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ input.addEventListener('input', (e) => {
 });
 
 const
-  acronyms = ['ID', 'URL', 'JSON', 'HTML', 'HTML5', 'PDF', 'IP', 'SMS', 'ISO', 'ZIP', 'AMP', 'ISP', 'OS', 'IOS', 'UTM', 'UTC', 'GDPR', 'API', 'VAT', 'IVR', 'MRR', 'PO', 'BCC', 'UBL', 'CSS', 'JS', 'CC'],
+  acronyms = ['ID', 'URL', 'JSON', 'HTML', 'HTML5', 'PDF', 'IP', 'SMS', 'ISO', 'ZIP', 'AMP', 'ISP', 'OS', 'IOS', 'UTM', 'UTC', 'GDPR', 'API', 'VAT', 'IVR', 'MRR', 'PO', 'BCC', 'UBL', 'CSS', 'CSS3', 'JS', 'CC'],
   articles = ['at', 'by', 'to', 'on', 'in', 'of', 'for', 'from', 'or', 'via', 'be', 'is', 'with'];
 
 function iterateObject(data) {
